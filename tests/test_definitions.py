eab90ff5dd6a925390e8bc272ea00a73d2581812
Daniel Stoops <dainelst@omnisys.co.il>
2025-02-11 10:00:23 +0200

    Add test_linspace_one_num()

--- tests/test_definitions.py
+++ tests/test_definitions.py
@@ -11,3 +11,7 @@ def test_no_end_linspace():
 
 def test_end_linspace():
     assert linspace(1, 10, 10) == [float(i) for i in range(1, 11)]
+
+
+def test_linspace_one_num():
+    assert linspace(1, 10, 1) == [1]
