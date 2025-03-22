762b0be89dd23333787a18fc836cb911c83fe438
Daniel Stoops <dainelst@omnisys.co.il>
2025-03-22 19:39:36 +0200

    Add test_is_hslaf() and test_is_hslf()

--- tests/test_identify.py
+++ tests/test_identify.py
@@ -1,4 +1,13 @@
-from colourings.identify import is_hsl, is_hsla, is_rgb, is_rgba, is_rgbaf, is_rgbf
+from colourings import Color
+from colourings.identify import (
+    is_hsl,
+    is_hsla,
+    is_hslaf,
+    is_rgb,
+    is_rgba,
+    is_rgbaf,
+    is_rgbf,
+)
 
 
 def test_bad_rbg():
@@ -41,3 +50,29 @@ def test_bad_hsla():
     assert not is_hsla((110, 200, 0, 0))
     assert not is_hsla("30, 300, 0, 0")
     assert not is_hsla(int)
+
+
+def test_is_hslaf():
+    assert is_hslaf((0, 0, 0, 0))
+    assert is_hslaf(Color(hslaf=(0, 0, 0, 0)).hsla)
+    assert not is_hslaf((2, 0, 0, 0))
+    assert not is_hslaf((0, 2, 0, 0))
+    assert not is_hslaf((0, 0, 2, 0))
+    assert not is_hslaf((0, 0, 0, 2))
+    assert not is_hslaf((-2, 0, 0, 0))
+    assert not is_hslaf((0, -2, 0, 0))
+    assert not is_hslaf((0, 0, -2, 0))
+    assert not is_hslaf((0, 0, 0, -2))
+
+
+def test_is_hslf():
+    assert is_hslaf((0, 0, 0))
+    assert is_hslaf(Color(hslaf=(0, 0, 0)).hsla)
+    assert not is_hslaf((2, 0, 0))
+    assert not is_hslaf((0, 2, 0))
+    assert not is_hslaf((0, 0, 2))
+    assert not is_hslaf((0, 0, 0))
+    assert not is_hslaf((-2, 0, 0))
+    assert not is_hslaf((0, -2, 0))
+    assert not is_hslaf((0, 0, -2))
+    assert not is_hslaf((0, 0, 0))
